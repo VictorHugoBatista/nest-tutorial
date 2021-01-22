d6f3075f76ad20e49ba89ecdde74f8164715d616
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-22 17:35:39 -0300

    Adiciona seed da tabela de usuários corretamente

--- mongo/mongo-init.js
+++ mongo/mongo-init.js
@@ -1,47 +1,7 @@
-print('Start #################################################################');
-
-db = db.getSiblingDB('api_prod_db');
-db.createUser(
-  {
-    user: 'api_user',
-    pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_prod_db' }],
-  },
-);
+db = db.getSiblingDB('customer-app');
 db.createCollection('users');
-
-db = db.getSiblingDB('api_dev_db');
-db.createUser(
-  {
-    user: 'api_user',
-    pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_dev_db' }],
-  },
-);
-db.createCollection('users');
-
-db = db.getSiblingDB('api_test_db');
-db.createUser(
-  {
-    user: 'api_user',
-    pwd: 'api1234',
-    roles: [{ role: 'readWrite', db: 'api_test_db' }],
-  },
-);
-db.createCollection('users');
-
-print('END #################################################################');
-
-// db.createCollection('meuuser2');
-// db.meuuser2.insert({
-//     "first_name": "teste4",
-//     "last_name": "teste4",
-//     "email": "teste4",
-//     "phone": "teste4",
-//     "address": "teste4",
-//     "description": "teste4",
-// });
-
-// console.log('aqui2')
-// mongo customer-app --authenticationDatabase admin -u root -p 123456 /docker-entrypoint-initdb.d/mongo-init.js
-// cat /docker-entrypoint-initdb.d/mongo-init.js
+db.users.insert({
+  name: 'Admin',
+  email: 'admin',
+  password: '123456',
+});
