40b1b4773f5aa416231ab03410536679d8f8de85
Victor Hugo Batista <vct.bta@gmail.com>
2021-01-25 09:29:14 -0300

    Adiciona users controller com rota /users/auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
   ],
-  providers: [UsersService]
+  providers: [UsersService],
+  controllers: [UsersController]
 })
 export class UsersModule {}
